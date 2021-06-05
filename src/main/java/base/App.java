d795ac68b2de2f94153b3be7299c38876e3d94fd
xera51 <xera51@protonmail.com>
2021-06-05 12:28:17 -0400

    Separated processing from output generation.

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -15,8 +15,9 @@ public class App {
 
         int num1 = myApp.readNum("first");
         int num2 = myApp.readNum("second");
+        int[] answers = myApp.performOperations(num1, num2);
 
-        String output = myApp.generateOutputString(num1, num2);
+        String output = myApp.generateOutputString(num1, num2, answers);
         myApp.displayOutput(output);
     }
 
@@ -25,10 +26,21 @@ public class App {
         return Integer.parseInt(in.nextLine());
     }
 
-    public String generateOutputString(int num1, int num2) {
+    public int[] performOperations(int num1, int num2) {
+        int[] answers = new int[4];
+
+        answers[0] = num1 + num2;
+        answers[1] = num1 - num2;
+        answers[2] = num1 * num2;
+        answers[3] = num1 / num2;
+
+        return answers;
+    }
+
+    public String generateOutputString(int num1, int num2, int[] answers) {
         return String.format("%d + %d = %d%n%d - %d = %d%n%d * %d = %d%n%d / %d = %d%n",
-                num1, num2, num1 + num2, num1, num2, num1 - num2,
-                num1, num2, num1 * num2, num1, num2, num1 / num2);
+                num1, num2, answers[0], num1, num2, answers[1],
+                num1, num2, answers[2], num1, num2, answers[3]);
     }
 
     public void displayOutput(String output) {
--- src/test/java/base/AppTest.java
+++ src/test/java/base/AppTest.java
@@ -10,13 +10,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
-    public void output_string_is_properly_formatted() {
+    public void calculations_are_performed_correctly() {
         App myApp = new App();
         int testNum1 = 10;
         int testNum2 = 5;
 
+        int[] expectedOutput = {15, 5, 50, 2};
+        int[] actualOutput = myApp.performOperations(testNum1, testNum2);
+
+        assertArrayEquals(expectedOutput, actualOutput);
+
+    }
+
+
+
+    @Test
+    public void output_string_is_properly_formatted() {
+        App myApp = new App();
+        int testNum1 = 10;
+        int testNum2 = 5;
+        int[] testAnswers = {15, 5, 50, 2};
+
         String expectedOutput = String.format("10 + 5 = 15%n10 - 5 = 5%n10 * 5 = 50%n10 / 5 = 2%n");
-        String actualOutput = myApp.generateOutputString(testNum1, testNum2);
+        String actualOutput = myApp.generateOutputString(testNum1, testNum2, testAnswers);
 
         assertEquals(expectedOutput, actualOutput);
     }
